dc9067109dd97e29de5eca6d5ad525b55bf29e23
Ian Bates <id_bates@yahoo.co.uk>
2019-09-13 16:24:29 +0100

    + refined docs and layout

---
+++ TelerikDemo/Controllers/TelerikAjaxStaticGridController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikDemo.Models;
+
+namespace TelerikDemo.Controllers
+{
+    public class TelerikAjaxStaticGridController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
